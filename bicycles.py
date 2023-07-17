292c77131dfe871d77477ea0da6074ec5faa6552
OLIORG <grootaerdtOliver@gmail.com>
2023-07-17 12:26:25 +0800

    add oef h4

---
+++ bicycles.py
@@ -0,0 +1,100 @@
+eten='Pizza'
+auto='Tesla'
+Pizza='pizza bbq chiken & bacon'
+muziek='Piano'
+
+
+print("Is eten == 'Pizza'? i 000000redict True.")
+
+
+print("Is auto == 'Tesla'? i predict True.")
+print(auto.lower() == 'tesla') 
+
+print("Is Pizza == 'pizza bbq chiken & bacon'? i predict True.")
+print(Pizza == 'pizza bbq chiken & bacon') 
+
+print("Is muziek == 'Piano'? i predict True.")
+print(muziek == 'Piano') 
+
+
+print("\nIs eten == 'banaan'? I predict false.")
+print(eten =='banaan')
+
+print("\nIs auto == 'Audi'? I predict false.")
+print(auto =='Audi')
+
+print("\nIs Pizza == 'pizza bbq chiken '? i predict false.")
+print(Pizza == 'pizza bbq chiken')
+
+print("nIs muziek == 'piano bbq '? i predict false.")
+print(Pizza == 'piano bbq')
+
+age = 19
+if age >= 18:
+	print("you can vote")
+
+
+
+
+
+
+
+alien_color='green' 'yellow' 'pink'
+
+if 'green' in alien_color:
+	print("dat is juist +5 punten")
+	
+if not'red' in alien_color:
+	print("fout")
+	
+
+
+
+
+alien_color2='green' 'purple' 'blue'
+
+if 'green' in alien_color2:
+	print("je hebt 5 punten verdiend door de alien neer te schieten")
+	
+if not 'red' in alien_color2:
+
+	print("je hebt 10 punten verdiend")
+else:
+	print ("fout")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+user='Nina','LUFU','Lozerfruit','beheerder','Oliver'
+if 'beheerder'==user:
+	print(hallo, beheerder)
+
---
+++ nina.py
@@ -0,0 +1,15 @@
+users= 'nina', 'Lozerfruit','LUFU','beheerder','oliver'
+for user in users:
+	if user == 'beheerder':
+		print('yo ' + user + ', kom binnen')
+	else:
+		print('hallo ' + user)
+
+user1 =[]
+if user1:
+	for user1 in users1:
+		print:(hallo)	
+else:
+	print('zoek naar nieuw medewerkers')
+
+
