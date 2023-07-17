cd72148fc17af4abdc3f5d416d726c0843005bea
OLIORG <grootaerdtOliver@gmail.com>
2023-07-17 16:58:57 +0800

    Create 7_2_ restaurant.py

---
+++ 7_2_ restaurant.py
@@ -0,0 +1,6 @@
+tafel= input("voor hoeveel mensen zal het zijn? ")
+tafel= int(tafel)
+if tafel <8 : 
+    print("volg maar") 
+else:
+    print("sorry we zitten vol je moet wachten tot er plaats is")
